ae1e4dda3436e0076fb90699c472242b86c3feae
Abdulmecid Demirhan <abdulmecid@cross4cloud.com>
2023-01-15 00:26:59 +0300

    DisplayTasks function created.

---
+++ script.js
@@ -0,0 +1,20 @@
+let todoList = [
+   {
+    "id": 1,
+    "assignName": "Something 1",
+    "situation": "completed"
+   },
+   {
+    "id": 2,
+    "assignName": "Something 2",
+    "situation": "pending"
+   },
+   {
+    "id": 3,
+    "assignName": "Something 3",
+    "situation": "pending"
+   }
+];
+
+function callTodo() {
+}

db1f6f19f8b3ebbd50b32d76067de499ce0c7a13
Abdulmecid Demirhan <abdulmecid@cross4cloud.com>
2023-01-15 00:56:29 +0300

    newTask() function created.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let addTodoButton = document.getElementById('addTodo');
 let inputTodo = document.getElementById('task');
-let inputText = document.getElementById('addTodoInput');
 let removeButton = document.getElementById('removeButton');
 
 let todoList = [
@@ -8,14 +7,29 @@ let todoList = [
  {"id":2, "todoName": "Talking"},
  {"id":3, "todoName": "Walking"}
 ]
-
+let alertBox = document.getElementById("alert");
 displayTasks();
+
+document.querySelector("#addTodo").addEventListener('click', newTask);
+
+function newTask(){
+   let taskInput = document.querySelector("#addTodoInput");
+   if(taskInput.value == ""){
+      
+      alertBox.style.display = "block";
+   }else{
+      todoList.push({"id": todoList.length + 1, "todoName": taskInput.value});
+      taskInput.value = "";
+      displayTasks();
+      alertBox.style.display = "none";
+   }
+};
+
 function displayTasks() {
    let ul = document.getElementById('taskList');
    ul.innerHTML = "";
 
-
-      for(let todo of todoList) {
+   for(let todo of todoList) {
          let li = `
          <li class="task" id="task">
                         <input class="form-check-input" type="checkbox" value="" id="${todo.id}"> ${todo.todoName}
@@ -27,5 +41,6 @@ function displayTasks() {
  ul.insertAdjacentHTML("beforeend", li);
       }
 };
+
 
  

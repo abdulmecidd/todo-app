d8c800e8ed1fb57aa9701a4b66bfb7a3325e65ab
Abdulmecid Demirhan <abdulmecid@cross4cloud.com>
2023-01-15 00:34:02 +0300

    displaytasks() created.

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-let todoList = [
-   {
-    "id": 1,
-    "assignName": "Something 1",
-    "situation": "completed"
-   },
-   {
-    "id": 2,
-    "assignName": "Something 2",
-    "situation": "pending"
-   },
-   {
-    "id": 3,
-    "assignName": "Something 3",
-    "situation": "pending"
-   }
-];
+let addTodoButton = document.getElementById('addTodo');
+let inputTodo = document.getElementById('task');
+let inputText = document.getElementById('addTodoInput');
+let removeButton = document.getElementById('removeButton');
 
-function callTodo() {
-}
+let todoList = [
+ {"id":1, "todoName": "Writing"},
+ {"id":2, "todoName": "Talking"},
+ {"id":3, "todoName": "Walking"}
+]
+
+displayTasks();
+function displayTasks() {
+   let ul = document.getElementById('taskList');
+   ul.innerHTML = "";
+
+
+      for(let todo of todoList) {
+         let li = `
+         <li class="task" id="task">
+                        <input class="form-check-input" type="checkbox" value="" id="${todo.id}"> ${todo.todoName}
+                        <label class="form-check-label" for="${todo.id}">
+                          </label>
+                       </li>
+                            
+         `;
+ ul.insertAdjacentHTML("beforeend", li);
+      }
+};
+
+ 

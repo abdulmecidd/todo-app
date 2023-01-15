ad03af2027a5ed85534dc111a62156eed7976630
Abdulmecid Demirhan <abdulmecid@cross4cloud.com>
2023-01-15 16:27:53 +0300

    Dropdown items (edit and delete) added.

--- script.js
+++ script.js
@@ -30,11 +30,22 @@ function displayTasks() {
 
    for(let todo of todoList) {
          let li = `
-         <li class="task" id="task">
-                        <input class="form-check-input" type="checkbox" value="" id="${todo.id}"> ${todo.todoName}
-                        <label class="form-check-label" for="${todo.id}">
-                          </label>
-                       </li>
+         <li class="task list-group-item" id="task">
+         <div class="form-check">
+               <input class="form-check-input" type="checkbox" value="" id="${todo.id}"> ${todo.todoName}
+               <label class="form-check-label" for="${todo.id}">
+               </label>
+               </div>
+               <div class="dropdown">
+               <button class="btn btn-link dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                     <i class="fa-solid fa-ellipsis"></i>
+               </button>
+               <ul class="dropdown-menu">
+                     <li><a onclick="deleteTask(${todo.id})" class="dropdown-item" href="#"><i class="fa-solid fa-trash"></i> Delete</a></li>
+                     <li><a onclick='editTask(${todo.id}, "${todo.gorevAdi}")' class="dropdown-item" href="#"><i class="fa-solid fa-pen"></i> Edit</a></li>
+               </ul>               
+               </div>
+          </li>
                             
          `;
  ul.insertAdjacentHTML("beforeend", li);
@@ -48,3 +59,7 @@ todoList.splice(0, todoList.length);
 displayTasks();
 
 };
+
+function updateTask() {
+
+}
